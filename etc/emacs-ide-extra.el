ddfcb216ab6fc6ba8826218462973dc1293e478c
Martin Vala <Martin.Vala@cern.ch>
2013-07-22 11:07:00 +0400

    Fixed intend for emacs ide from 4 to 3

--- etc/emacs-ide-extra.el
+++ etc/emacs-ide-extra.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eide-custom-c-indent-offset 4))
+ '(eide-custom-c-indent-offset 3))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
