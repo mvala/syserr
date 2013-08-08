e9d7da8aaf921165c913e8f8a22acfd90f0f2209
Martin Vala <Martin.Vala@cern.ch>
2013-08-08 12:26:51 +0400

    save for presentation

---
+++ run_rsn_sys_err_bad.C
@@ -0,0 +1,100 @@
+#ifndef __CINT__
+#include <TROOT.h>
+#include "TSysError.h"
+#include "TSysErrorUtils.h"
+#endif
+
+void ProcessDirecotry(TObject *se, const char *dirname, const char *filter="", Int_t maxMeasurments=kMaxInt,
+                      Int_t maxMethods = kMaxInt,
+                      TString refMC="$HOME/Documents/Work/Presentations/2013/RESONANCE_2013-08-09/analysis/2013-08-07/SYS_ERR/00_REF/KTPCnsig30_STD2010_PRIMARY_00")
+{
+
+   TString pwd = gSystem->WorkingDirectory();
+   const char *dir = gSystem->ExpandPathName(dirname);
+   const char *refmc = gSystem->ExpandPathName(refMC);
+
+   if (!gSystem->ChangeDirectory(dir)) {
+      ::Error("TSysError::AddGraphDirectory",
+              TString::Format("Could not enter direcotry '%s' !!!", dir).Data());
+      return kFALSE;
+   }
+   TString out = gSystem->GetFromPipe(TString::Format("ls -1 %s", filter).Data());
+   if (out.IsNull()) {
+      ::Error("TSysError::AddGraphDirectory",
+              TString::Format("No files found in '%s' !!!", dir).Data());
+      return kFALSE;
+
+   }
+
+   
+   TString outRefMC = gSystem->GetFromPipe(TString::Format("ls -1 %s%s", refMC.Data(), filter).Data());
+   if (outRefMC.IsNull()) {
+      ::Error("TSysError::AddGraphDirectory",
+              TString::Format("RefMC: No files found in '%s' !!!", dir).Data());
+      return kFALSE;
+
+   }
+   
+   TObjArray *t = out.Tokenize("\n");
+   TObjArray *t2 = outRefMC.Tokenize("\n");
+   TObjString *so, *so2;
+   TString s,s2;
+   TIter next(t);
+   TSysError *seTmp;
+   Int_t c=0;
+   TList *l;
+   while ((so = (TObjString *) next())) {
+      s = so->GetString();
+      seTmp = new TSysError(s.Data(), "");
+      seTmp->AddGraphDirectory(TString::Format("%s/%s/", dir,s.Data()).Data(), "RFE", "%lg %lg %lg %lg", maxMeasurments);
+      seTmp->SetType(TSysError::kMean);
+      seTmp->SetTypeToList(TSysError::kRelativeErrorMC);
+      ((TSysError*)se)->Add(seTmp);
+      TIter nextSE(seTmp->GetList());
+      TH1D *h;
+      Int_t c2=0;
+      while ((seTmp = (TSysError *) nextSE())) {
+         so2 = (TObjString *) t2->At(c2); 
+         s2 = so2->GetString();
+         seTmp->AddInput(new TNamed("RefMC", TString::Format("%s/RFE",refMC.Data()).Data()));
+         c2++;
+      }
+      if (++c>=maxMethods) break;
+   }
+   gSystem->ChangeDirectory(pwd.Data());
+}
+
+void run_rsn_sys_err_bad()
+{
+
+   gROOT->ProcessLine(".L TSysErrorUtils.cxx+g");
+   gROOT->ProcessLine(".L TSysError.cxx+g");
+   if (!gROOT->GetClass("TSysError")) return;
+
+   Bool_t rc;
+
+   TString dir;
+
+   // dir = "/eos/saske.sk/scratch/ALICE/RSN_OUT/001";
+   dir = "$HOME/Documents/Work/Presentations/2013/RESONANCE_2013-08-09/analysis/2013-08-07/SYS_ERR/01_QUALITY/";
+
+   TSysError *bestMethodNumMCPt = new TSysError("bestMethodNumMCPt", "Best Method in num MC Pt");
+   // bestMethodNumMCPt->SetType(TSysError::kMean);
+   bestMethodNumMCPt->SetType(TSysError::kMinStdDev);
+   // bestMethodNumMCPt->SetType(TSysError::kMinStdDev);
+   bestMethodNumMCPt->SetPrintInfo(kTRUE);
+
+   // ProcessDirecotry(bestMethodNumMCPt, dir.Data(),"",kMaxInt,2);
+   ProcessDirecotry(bestMethodNumMCPt, dir.Data(),"",kMaxInt,kMaxInt);
+
+
+   rc = bestMethodNumMCPt->Calculate();
+
+   Printf("Process status : %s", (rc == 0) ? "FAILED !!!" : "OK");
+
+   // TGraphErrors *gr = finalPt->GetGraph();
+   // if (!gr) return;
+   // Printf("Drawing graph ...");
+   // gr->Draw("APE");
+
+}
